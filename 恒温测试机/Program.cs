6e3e6bf573ca6f67e9f71cd7e834d400c50026d4
monkcheng <chengzijun@iie.ac.cn>
2019-09-16 18:29:25 +0800

    添加hsl控件库的注册代码

--- 恒温测试机/Program.cs
+++ 恒温测试机/Program.cs
@@ -15,6 +15,16 @@ namespace 恒温测试机
         [STAThread]
         static void Main()
         {
+            // 注册控件示例，如果注册失败，你的控件仍然只能使用8个小时
+            bool isSuccess = HslControls.Authorization.SetAuthorizationCode("d906733d-b7d9-477d-8773-c0d1a86ef7da");
+            if (!isSuccess)
+            {
+                Console.WriteLine("注册失败");
+            }
+            else
+            {
+                Console.WriteLine("注册成功");
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
